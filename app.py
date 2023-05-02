ca07833e7eaacb830ff5b96ce2eeb353d8e0687d
Giridhar Reddy Gangireddy <ggangire@buffalo.edu>
2023-05-02 16:25:53 -0400

    16 commit

--- app.py
+++ app.py
@@ -8,14 +8,6 @@ from pathlib import Path
 app = Flask(__name__)
 app.config["APPLICATION_ROOT"] = "/api/"
 
-# model = Sequential()
-# model.add(Dense(8,input_dim=7,activation="relu"))
-# model.add(Dense(5,activation='relu'))
-
-# model.add(Dense(3,activation='relu'))
-# model.add(Dense(1,activation='sigmoid'))
-
-
 THIS_FOLDER = Path(__file__).parent.resolve()
 
 model = get_model()
@@ -23,11 +15,6 @@ model = get_model()
 weights_file = THIS_FOLDER / "weights.h5"
 
 model.load_weights(weights_file)
-
-# from flask import Flask, jsonify
-
-# app = Flask(__name__)
-# app.config["APPLICATION_ROOT"] = "/api/"
 
 @app.route('/')
 def home():
@@ -45,41 +32,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# from flask import Flask, jsonify, request, render_template
-# from model import get_model
-
-# from pathlib import Path
-
-# app = Flask(__name__)
-# app.config["APPLICATION_ROOT"] = "/api/"
-
-
-# # THIS_FOLDER = Path(__file__).parent.resolve()
-
-# # model = get_model()
-
-# # weights_file = THIS_FOLDER / "weights.h5"
-
-# # model.load_weights(weights_file)
-
-
-# @app.route('/')
-# def home():
-#     return jsonify("helloworld")
-#     # return render_template("ui.html")
-
-# @app.route('/predict', methods = ['POST'])
-# def predict():
-#     # input_data = [[float(i) for i in row] for row in request.json['input']]
-#     # y_pred_val = model.predict(input_data)
-#     # y_pred_val_labels = (y_pred_val >= 0.5).astype(int)
-#     # for i in range(len(input_data)):
-#     #     input_data[i].append(int(y_pred_val_labels[i][0]))
-#     # return jsonify(input_data)
-
-#     return jsonify("hi")
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
