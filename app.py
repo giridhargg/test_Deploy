d04d3b99c15961518f60e11a6cb83d8ec075f811
Giridhar Reddy Gangireddy <ggangire@buffalo.edu>
2023-05-02 15:00:25 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify("hello world")
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    return jsonify("hello world")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
