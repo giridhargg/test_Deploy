5de1854100448cd2426cb34cbe8b6abdb226e5f1
Giridhar Reddy Gangireddy <ggangire@buffalo.edu>
2023-05-02 15:35:18 -0400

    10 commit

--- app.py
+++ app.py
@@ -24,13 +24,13 @@ app = Flask(__name__)
 app.config["APPLICATION_ROOT"] = "/api/"
 
 
-THIS_FOLDER = Path(__file__).parent.resolve()
+# THIS_FOLDER = Path(__file__).parent.resolve()
 
-model = get_model()
+# model = get_model()
 
-weights_file = THIS_FOLDER / "weights.h5"
+# weights_file = THIS_FOLDER / "weights.h5"
 
-model.load_weights(weights_file)
+# model.load_weights(weights_file)
 
 
 @app.route('/')
@@ -40,12 +40,14 @@ def home():
 
 @app.route('/predict', methods = ['POST'])
 def predict():
-    input_data = [[float(i) for i in row] for row in request.json['input']]
-    y_pred_val = model.predict(input_data)
-    y_pred_val_labels = (y_pred_val >= 0.5).astype(int)
-    for i in range(len(input_data)):
-        input_data[i].append(int(y_pred_val_labels[i][0]))
-    return jsonify(input_data)
+    # input_data = [[float(i) for i in row] for row in request.json['input']]
+    # y_pred_val = model.predict(input_data)
+    # y_pred_val_labels = (y_pred_val >= 0.5).astype(int)
+    # for i in range(len(input_data)):
+    #     input_data[i].append(int(y_pred_val_labels[i][0]))
+    # return jsonify(input_data)
+
+    return jsonify("hi")
 
 
 if __name__ == '__main__':
